cb7d541965795a270e3451c136a646100c6ce203
Alex Ngun <alexngun7@gmail.com>
2021-07-09 21:41:00 +0800

    v1.0 release with demo

---
+++ demo.py
@@ -0,0 +1,15 @@
+#%%
+from DataExtraction.Regex import fieldExtractor
+from DataExtraction.Scanner import pdfScanner
+
+# %%
+pdf_dir = 'Demo Invoices/'
+template_dir = 'DataExtraction/Templates/'
+
+ocr = pdfScanner(pdf_dir)
+texts = ocr.extract_text()
+
+worker = fieldExtractor(template_dir, texts)
+data = worker.fetch()
+worker.save(data)
+# %%
